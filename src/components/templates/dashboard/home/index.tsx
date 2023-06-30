a6d0ef276567dd8a3a25c7055eba09b6b979c1f3
mertcancet <mertcancet95@gmail.com>
2023-06-30 18:05:36 +0300

    🔧 chore(package.json): add 'classnames' dependency ✨ feat(dashboard home): add styles and dashboard cards to display data 🎨 style(dashboard-card): add styles for different card variants

--- src/components/templates/dashboard/home/index.tsx
+++ src/components/templates/dashboard/home/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import styles from './styles.module.css';
+import { DashboardCard } from '@/components/ui';
+import { BookmarkPink, GraduationCapBlue, UsdSquareYellow, User } from '@/components/ui/icons';
+
 const DashboardHome = () => {
-  return <div>DashboardHome</div>;
+  return (
+    <div className={styles.container}>
+      <DashboardCard
+        variant="blue"
+        title="Students"
+        number={243}
+        icon={<GraduationCapBlue width={48} height={38} />}
+      />
+      <DashboardCard
+        variant="pink"
+        title="Course"
+        number={13}
+        icon={<BookmarkPink width={28} height={35} />}
+      />
+      <DashboardCard
+        variant="yellow"
+        title="Payment"
+        number="556,000₺"
+        icon={<UsdSquareYellow width={35} height={40} />}
+      />
+      <DashboardCard
+        variant="yellow-grad"
+        title="Users"
+        number={3}
+        icon={<User width={34} height={34} />}
+      />
+    </div>
+  );
 };
 
 export default DashboardHome;
--- src/components/ui/dashboard-card/index.tsx
+++ src/components/ui/dashboard-card/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
+import cx from 'classnames';
+
 import styles from './styles.module.css';
-import { GraduationCapBlue } from '../icons';
 
 type Props = {
   variant: 'blue' | 'pink' | 'yellow' | 'yellow-grad';
   icon: React.ReactNode;
-  title: 'string';
-  number: number;
+  title: string;
+  number: number | string;
 };
 
-const DashboardCard: React.FC<Props> = () => {
+const DashboardCard: React.FC<Props> = ({ icon, variant = 'blue', title, number }) => {
   return (
-    <div className={styles.container}>
-      <GraduationCapBlue />
-      <div className={styles.title}>Students</div>
-      <div className={styles.number}>243</div>
+    <div className={cx(styles.container, styles[variant])}>
+      {icon}
+      <div className={cx(styles.title, { [styles.white]: variant === 'yellow-grad' })}>{title}</div>
+      <div className={styles.number}>{number}</div>
     </div>
   );
 };
